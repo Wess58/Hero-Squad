6755bc40a89a7678fe37e0e55fd4feab59349547
Wess58 <wesleymbate@gmail.com>
2018-10-08 14:25:34 +0300

    created getter for index.vtl

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,11 +3,20 @@ import java.util.Map;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
+import java.util.ArrayList;
 
-public class App{
-    public static void main(String[] args){
+public class App {
+    public static void main(String[] args) {
         staticFileLocation("/public");
         String layout = "templates/layout.vtl";
+
+//Parse Index.vtl
+        get( "/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/index.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
 
     }
 }
